128cd53f3a97986d57601707950ace7ac6206350
HatmanStack <82614182+HatmanStack@users.noreply.github.com>
2025-10-28 16:45:28 +0000

    feat: export ConfigurationManager in ragstack_common package

---
+++ lib/ragstack_common/__init__.py
@@ -0,0 +1,10 @@
+"""RAGStack Common Library
+
+Shared utilities and classes for RAGStack-Lambda document processing pipeline.
+"""
+
+from ragstack_common.config import ConfigurationManager
+
+__all__ = [
+    'ConfigurationManager',
+]
